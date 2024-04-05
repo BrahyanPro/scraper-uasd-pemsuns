f8040f08b9df74c9c00fcd60042775a92080bec4
Brahyan Martinez <brahyanpro16@gmail.com>
2024-04-04 22:54:12 -0400

    Ready for discotecta

--- app.js
+++ app.js
@@ -1,22 +1,71 @@
 const playwright = require('playwright');
 const fs = require('fs').promises;
+const path = require('path');
 
-(async () => {
-    // for (const browserType of ['chromium', 'firefox', 'webkit']) {
-    //     const browser = await playwright[browserType].launch();
-    //     const context = await browser.newContext();
-    //     const page = await context.newPage();
-    //     await page.goto("https://github.com/BrahyanPro");
-    //     await page.screenshot({path: `brahyanpro${browserType}.png`, fullPage: true});
-    //     await page.waitForTimeout(1000);
-    //     await browser.close();
-    // };
+const folderPath = path.join(__dirname, 'carreras');
 
+
+// Se crea la carpeta 'carreras' y hacemos que exista  si o si puta
+fs.mkdir(folderPath, { recursive: true }).catch(console.error);
+
+async function extractCareerLinks() {
     const browser = await playwright.chromium.launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto("https://soft.uasd.edu.do/PensumGrado/?periodoV=999999&programa=P-APLA&plan=200820&nivel=GR", { waitUntil: 'networkidle' });
-    await page.screenshot({path: `softuasd.png`, fullPage: true});
-    await page.waitForTimeout(1000);
+    const page = await browser.newPage();
+
+    await page.goto('https://soft.uasd.edu.do/planesgrado/');
+
+    const careerLinks = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('.tvCarreras_0[target="_blank"]')).map(link => ({
+            title: link.innerText,
+            url: link.href,
+        }));
+    });
+
+    for (const career of careerLinks) {
+        const data = await savePensumData(browser, career);
+        await fs.writeFile(path.join(folderPath, `${data.name}.json`), JSON.stringify(data.data, null, 2));
+    }
+
+    console.log('Todos los datos del pemsun han sido guardados.');
     await browser.close();
-})();
+}
+
+
+const savePensumData = async (browser, carrera) => {
+
+    const page = await browser.newPage();
+    await page.goto(carrera.url, { waitUntil: 'networkidle' });
+
+    const data = await page.evaluate(() => {
+        const rows = Array.from(document.querySelectorAll('#datosPrincipales tr'));
+        let semester = '';
+        const tableData = [];
+
+        for (const row of rows) {
+            if (row.cells.length === 1) {
+                semester = row.innerText.trim();
+            } else if (row.cells.length > 1) {
+                const [clave, asignatura, ht, hp, cr, prerequisitos, equivalencias] = Array.from(row.cells, cell => cell.innerText.trim());
+                tableData.push({
+                    semester,
+                    clave,
+                    asignatura,
+                    ht,
+                    hp,
+                    cr,
+                    prerequisitos,
+                    equivalencias,
+                });
+            }
+        }
+        return tableData;
+    });
+
+    // Formatear el título de la carrera para el nombre del archivo
+    const title = carrera.title.replace(/ /g, '_').toLowerCase();
+    return { data, name: title };
+}
+
+extractCareerLinks().catch(console.error);
+// TODO Hacerlo mas optimo rendimiento actual por debajo de la media
+
