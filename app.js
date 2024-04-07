6ffa4d63cd122529d77387ee3cec024031e010d2
Brahyan Martinez <brahyanpro16@gmail.com>
2024-04-07 17:34:52 -0400

    Probando traerme data formateada y linda

--- app.js
+++ app.js
@@ -1,71 +1,96 @@
-const playwright = require('playwright');
-const fs = require('fs').promises;
-const path = require('path');
+const playwright = require('playwright')
+const fs = require('fs').promises
+const path = require('path')
 
-const folderPath = path.join(__dirname, 'carreras');
+const folderPath = path.join(__dirname, 'carreras')
+fs.mkdir(folderPath, { recursive: true }).catch(console.error)
+
+async function extractCareerLinks () {
+  const browser = await playwright.chromium.launch()
+  const page = await browser.newPage()
+  await page.goto('https://soft.uasd.edu.do/planesgrado/')
 
 
-// Se crea la carpeta 'carreras' y hacemos que exista  si o si puta
-fs.mkdir(folderPath, { recursive: true }).catch(console.error);
-
-async function extractCareerLinks() {
-    const browser = await playwright.chromium.launch();
-    const page = await browser.newPage();
-
-    await page.goto('https://soft.uasd.edu.do/planesgrado/');
-
-    const careerLinks = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('.tvCarreras_0[target="_blank"]')).map(link => ({
-            title: link.innerText,
-            url: link.href,
-        }));
+  const faculties = await page.$$eval('div#tvCarreras div', (divs) => {
+    return divs.map((div) => {
+      const facultyName = div.querySelector('a.tvCarreras_0').innerText;
+      const schools = div.querySelectorAll('table > tbody > tr > td > a');
+      const schoolData = Array.from(schools).map((school) => {
+        const schoolName = school.innerText;
+        // Further extract programs and URLs within each school
+        return {
+          name: schoolName,
+          // programs: Iterate and extract programs similar to schools
+        };
+      });
+      return {
+        faculty: facultyName,
+        schools: schoolData,
+      };
     });
+  });
 
-    for (const career of careerLinks) {
-        const data = await savePensumData(browser, career);
-        await fs.writeFile(path.join(folderPath, `${data.name}.json`), JSON.stringify(data.data, null, 2));
-    }
+  console.log(faculties);
 
-    console.log('Todos los datos del pemsun han sido guardados.');
-    await browser.close();
+  // const careerLinks = await page.evaluate(() => {
+  //   return Array.from(
+  //     document.querySelectorAll('.tvCarreras_0[target="_blank"]')).
+  //     map(link => ({
+  //       title: link.innerText,
+  //       url: link.href,
+  //     }))
+  // })
+
+
+
+  // for (const career of careerLinks) {
+  //   const data = await savePensumData(browser, career)
+  //   await fs.writeFile(
+  //     path.join(folderPath, `${data.name}.json`),
+  //     JSON.stringify(data.data, null, 2),
+  //   )
+  // }
+
+  console.log('Todos los datos del pemsun han sido guardados.')
+  await browser.close()
 }
-
 
 const savePensumData = async (browser, carrera) => {
+  const page = await browser.newPage()
+  await page.goto(carrera.url, { waitUntil: 'networkidle' })
 
-    const page = await browser.newPage();
-    await page.goto(carrera.url, { waitUntil: 'networkidle' });
+  const data = await page.evaluate(() => {
+    const rows = Array.from(document.querySelectorAll('#datosPrincipales tr'))
+    let semester = ''
+    const tableData = []
 
-    const data = await page.evaluate(() => {
-        const rows = Array.from(document.querySelectorAll('#datosPrincipales tr'));
-        let semester = '';
-        const tableData = [];
+    for (const row of rows) {
+      if (row.cells.length === 1) {
+        semester = row.innerText.trim()
+      } else if (row.cells.length > 1) {
+        const [clave, asignatura, ht, hp, cr, prerequisitos, equivalencias] = Array.from(
+          row.cells,
+          cell => cell.innerText.trim(),
+        )
+        tableData.push({
+          semester,
+          clave,
+          asignatura,
+          ht,
+          hp,
+          cr,
+          prerequisitos,
+          equivalencias,
+        })
+      }
+    }
+    return tableData
+  })
 
-        for (const row of rows) {
-            if (row.cells.length === 1) {
-                semester = row.innerText.trim();
-            } else if (row.cells.length > 1) {
-                const [clave, asignatura, ht, hp, cr, prerequisitos, equivalencias] = Array.from(row.cells, cell => cell.innerText.trim());
-                tableData.push({
-                    semester,
-                    clave,
-                    asignatura,
-                    ht,
-                    hp,
-                    cr,
-                    prerequisitos,
-                    equivalencias,
-                });
-            }
-        }
-        return tableData;
-    });
-
-    // Formatear el título de la carrera para el nombre del archivo
-    const title = carrera.title.replace(/ /g, '_').toLowerCase();
-    return { data, name: title };
+  // Formatear el título de la carrera para el nombre del archivo
+  const title = carrera.title.replace(/ /g, '_').toLowerCase()
+  return { data, name: title }
 }
 
-extractCareerLinks().catch(console.error);
+extractCareerLinks().catch(console.error)
 // TODO Hacerlo mas optimo rendimiento actual por debajo de la media
-
